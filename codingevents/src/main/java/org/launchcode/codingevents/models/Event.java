6237a3b8b9fa10b03ffd69ae2ebc5cd5e8b64d1a
sharee-tech <mail.sharee@gmail.com>
2023-10-19 11:00:12 -0500

    Created a persistent model in thymeleaf application

--- codingevents/src/main/java/org/launchcode/codingevents/models/Event.java
+++ codingevents/src/main/java/org/launchcode/codingevents/models/Event.java
@@ -3,7 +3,6 @@ package org.launchcode.codingevents.models;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
-
 import java.util.Objects;
 
 public class Event {
