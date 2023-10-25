9b9616feb90f197e8bf08245c6d46ff43cb1bc67
sharee-tech <mail.sharee@gmail.com>
2023-10-25 13:57:24 -0500

    One to many relationship

--- coding-events-demo/src/main/java/org/launchcode/codingevents/models/EventCategory.java
+++ coding-events-demo/src/main/java/org/launchcode/codingevents/models/EventCategory.java
@@ -3,7 +3,10 @@ package org.launchcode.codingevents.models;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
@@ -15,6 +18,9 @@ public class EventCategory extends AbstractEntity {
 
     @Size(min=3, message="Name must be at least 3 characters long")
     private String name;
+
+    @OneToMany(mappedBy = "eventCategory")
+    private final List<Event> events = new ArrayList<>();
 
     public EventCategory(@Size(min = 3, message = "Name must be at least 3 characters long") String name) {
         this.name = name;
@@ -32,6 +38,10 @@ public class EventCategory extends AbstractEntity {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public List<Event> getEvents() {
+        return events;
     }
 
     @Override
